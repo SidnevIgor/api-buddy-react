9572ab68b51457dd9dd00127cfccd92150918fd9
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-09-15 16:46:15 +0300

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,22 @@
 import React, {Component} from 'react';
 
 class Counter extends Component {
+  state = {
+    count: 0
+  }
   render() {
     return (
       <div>
-        <h2>This is a counter</h2>
-        <button>Increment</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm" onClick={this.increment}>Increment</button>
       </div>
     );
+  }
+  increment = () => {
+    this.setState({count: this.state.count + 1});
+  }
+  formatCount() {
+    return this.state.count === 0 ? 'Zero' : this.state.count;
   }
 }
 
