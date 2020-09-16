5e30e58cceb83635fb1f52b0f5edb0ca25ed4b91
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-09-16 11:21:57 +0300

    adding App component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
+import 'font-awesome/css/font-awesome.css';
 
+import App from './components/app';
 import Counter from './components/counter';
 
 
-ReactDOM.render(<Counter/>, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
