1ead6f99ce048a7e11af1b55ba5e612ec0d28f02
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-09-17 18:33:45 +0300

    removing fetch run

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,10 @@
 import React, {Component} from 'react';
 
-import fetchData from '../services/fetchData';
-
-
 class App extends Component {
   render() {
-    this.getData();
     return (
       <div>Hello</div>
     );
-  }
-  getData() {
-    fetchData('employees/1','DELETE').then(data => {
-      console.log(data);
-    });
   }
 }
 export default App;
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,28 +5,23 @@ class Header extends Component {
   render() {
     return (
       <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-      <a className="navbar-brand" href="#">Navbar</a>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault" aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-
       <div className="collapse navbar-collapse" id="navbarsExampleDefault">
         <ul className="navbar-nav mr-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+            <a className="nav-link" >Home <span className="sr-only">(current)</span></a>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">Link</a>
+            <a className="nav-link" >Link</a>
           </li>
           <li className="nav-item">
-            <a className="nav-link disabled" href="#">Disabled</a>
+            <a className="nav-link disabled" >Disabled</a>
           </li>
           <li className="nav-item dropdown">
             <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
             <div className="dropdown-menu" aria-labelledby="dropdown01">
-              <a className="dropdown-item" href="#">Action</a>
-              <a className="dropdown-item" href="#">Another action</a>
-              <a className="dropdown-item" href="#">Something else here</a>
+              <a className="dropdown-item" >Action</a>
+              <a className="dropdown-item" >Another action</a>
+              <a className="dropdown-item" >Something else here</a>
             </div>
           </li>
         </ul>
