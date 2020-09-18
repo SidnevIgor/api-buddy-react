8ad0b8cc0b58888305228d946925c871502151e7
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-09-18 21:18:17 +0300

    Deleting data from table

--- src/components/table-ex.jsx
+++ src/components/table-ex.jsx
@@ -7,9 +7,11 @@ class Table extends Component {
     customers: []
   };
   render() {
-    fetchData('customers','GET').then(data => {
-      this.setState({customers: data});
-    });
+    if(this.state.customers.length === 0) {
+      fetchData('customers','GET').then(data => {
+        this.setState({customers: data});
+      });
+    }
     return (
     <div>
       <h1>There are {this.state.customers.length} customers</h1>
@@ -23,19 +25,24 @@ class Table extends Component {
           </tr>
         </thead>
         <tbody>
-            { this.state.customers.map( (customer, i) => { return (
+            {this.state.customers.map((customer, i) => { return (
               <tr key={i}>
               <td key={i+1}>{customer.customerId}</td>
               <td key={i+2}>{customer.firstName}</td>
               <td key={i+3}>{customer.lastName}</td>
               <td key={i+4}>{customer.email}</td>
+              <td><button className="btn btn-danger" onClick={() => this.deleteRow(i)}>Delete</button></td>
               </tr>
-            )
-            }) }
+            )})}
         </tbody>
       </table>
     </div>
     );
+  }
+  deleteRow = (id) => {
+    let newArr = [...this.state.customers];
+    newArr.splice(id,1);
+    this.setState({customers: newArr});
   }
 }
 export default Table;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 
-import App from './components/app';
+//import App from './components/app';
 import Table from './components/table-ex';
 
 ReactDOM.render(<Table/>, document.getElementById('root'));
