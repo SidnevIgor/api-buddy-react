769bbbf47f5a60819f9d6942579860514343d27d
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-09-18 19:13:14 +0300

    Update fetchData.js

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,5 +1,5 @@
 async function fetchData(route, method, data) {
-  let url = 'https://api-buddy.herokuapp.com/api/'+route;
+  let url = 'https://api-buddy.herokuapp.com/api/' + route;
   const response = await fetch(url, {
     method: method, // *GET, POST, PUT, DELETE, etc.
     headers: { 'Content-Type': 'application/json' },
