43bc9fb6a81f5d6ecf5cb96d3224a19a4fc90e30
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-09-22 16:30:26 +0300

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,6 +5,15 @@ export default class Counter extends Component {
     count: 0
   };
   render() {
-    return (<h1>Check</h1>);
+    return (
+      <div>
+        <span className="text-success m-3">{this.state.count}</span>
+        <button onClick = {() => this.increment() }>Add</button>
+      </div>
+    );
+  }
+  increment = () => {
+
+    this.setState({count: ++this.state.count});
   }
 }
