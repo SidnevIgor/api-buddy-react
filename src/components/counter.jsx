b96f54f566f99955d83cfb96d0703fc7aeb30e1d
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-09-22 17:37:50 +0300

    bug fix

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,20 +2,20 @@ import React, {Component} from 'react';
 
 export default class Counter extends Component {
   state = {
-    count: this.props.value,
-    textClass: 'text-danger m-3'
+    count: this.props.value || 0
   };
   render() {
+    let textClass = 'm-3 bold text-';
+    textClass = textClass + (this.state.count === 0 ? 'warning' : 'success');
     return (
       <div>
-        <span className= {this.state.textClass}>{this.state.count}</span>
+        <span className= {textClass}>{this.state.count}</span>
         <button onClick = {() => this.increment() }>Add</button>
       </div>
     );
   }
   increment = () => {
     this.setState({
-      textClass: 'text-success m-3',
       count: ++this.state.count
     });
   }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,7 +4,7 @@ import Counter from './counter';
 export default class Counters extends Component {
   state = {
     counters: [
-      {id: 1, value: 1},
+      {id: 1, value: 0},
       {id: 2, value: 2},
       {id: 3, value: 3},
       {id: 4, value: 4}
