933524a6db09eed4081d261a9939527259093ff6
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-09-24 17:16:38 +0300

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,7 +11,7 @@ export default class Counter extends Component {
       <div>
         <span className= {textClass}>{this.props.count}</span>
         <button onClick = {() =>
-            this.props.onIncrement(this.props.counter) }>Add</button>
+        this.props.onIncrement(this.props.counter) }>Add</button>
         <button className="btn btn-danger m-2" onClick={() => this.props.onDelete(this.props.counter.id)}>Delete</button>
       </div>
     );
