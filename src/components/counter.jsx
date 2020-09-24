649c7586aa47efb6afd525abf2aecaf56c83da05
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-09-24 17:21:16 +0300

    Update counter.jsx

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,24 +1,16 @@
 import React, {Component} from 'react';
 
 export default class Counter extends Component {
-/*  state = {
-    count: this.props.counter.value || 0
-  };*/
   render() {
     let textClass = 'm-3 bold text-';
-    textClass = textClass + (this.state.count === 0 ? 'warning' : 'success');
+    textClass = textClass + (this.props.counter.value === 0 ? 'warning' : 'success');
     return (
       <div>
-        <span className= {textClass}>{this.props.count}</span>
+        <span className= {textClass}>{this.props.counter.value}</span>
         <button onClick = {() =>
         this.props.onIncrement(this.props.counter) }>Add</button>
         <button className="btn btn-danger m-2" onClick={() => this.props.onDelete(this.props.counter.id)}>Delete</button>
       </div>
     );
   }
-/*  increment = () => {
-    this.setState({
-      count: ++this.props.count
-    });
-  }*/
 }
