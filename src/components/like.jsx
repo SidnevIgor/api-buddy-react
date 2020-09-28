61e9596bb0393a904e9b1b2403eeabccb2d10cff
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-09-28 16:32:34 +0300

    Create like.jsx

---
+++ src/components/like.jsx
@@ -0,0 +1,12 @@
+var React, {Component} = require('react');
+
+export default class Like extends Component {
+  state = {
+    isChosen: false
+  };
+  render: function() {
+    return (
+      <div />
+    );
+  }
+}
