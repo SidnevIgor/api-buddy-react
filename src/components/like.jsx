8da5e097a2fdc9bd36eebe980b1b5421d5967026
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-09-28 16:54:49 +0300

    Install font-awesome replacements

--- src/components/like.jsx
+++ src/components/like.jsx
@@ -1,12 +1,14 @@
-var React, {Component} = require('react');
+import React, {Component} from 'react';
+import 'font-awesome/css/font-awesome.min.css';
 
 export default class Like extends Component {
   state = {
     isChosen: false
   };
-  render: function() {
+  render() {
     return (
-      <div />
+      <i className="far fa-heart" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-
+awesome.min.css"></i>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 
-import App from './components/app';
+//import App from './components/app';
 //import Table from './components/table-ex';
 //import Counter from './components/counter';
 //import Counters from './components/counters';
+import Like from './components/like';
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Like/>, document.getElementById('root'));
