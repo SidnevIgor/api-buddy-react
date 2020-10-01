5ceb9290d768e001ed5207c32e837be863983c80
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-01 12:09:56 +0300

    Adding 2 textboxes

--- src/components/fetching.jsx
+++ src/components/fetching.jsx
@@ -3,10 +3,15 @@ import React, {Component} from 'react';
 export default class Fetching extends Component {
   state = {
     items: []
-  }
+  };
   render() {
     return (
-      <h1>Fetch is here!</h1>
+      <React.Fragment>
+        <h1>Fetch is here!</h1>
+        <input type='text' style={{height: "200px", width: "150px"}} className="m-3"></input>
+        <input type='text' style={{height: "200px", width: "150px"}} className="m-3"></input>
+        <button style={{display: "block"}} className="m-3">Fetch data</button>
+      </React.Fragment>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import 'bootstrap/dist/css/bootstrap.css';
 import 'font-awesome/css/font-awesome.css';
 
 //import App from './components/app';
-import Table from './components/table-ex';
+//import Table from './components/table-ex';
 //import Counter from './components/counter';
 //import Counters from './components/counters';
 //import Like from './components/like';
+import Fetching from './components/fetching';
 
-ReactDOM.render(<Table/>, document.getElementById('root'));
+ReactDOM.render(<Fetching/>, document.getElementById('root'));
