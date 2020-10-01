4b5636328eb40632f956086e4fa04b5bd85dd469
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-01 11:29:54 +0300

    Update fetchData.js

--- src/services/fetchData.js
+++ src/services/fetchData.js
@@ -1,8 +1,11 @@
-async function fetchData(route, method, data) {
+async function fetchData(route, method, data, token) {
   let url = 'https://api-buddy.herokuapp.com/api/' + route;
   const response = await fetch(url, {
     method: method, // *GET, POST, PUT, DELETE, etc.
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+      'x-auth-token': token
+    },
     body: JSON.stringify(data) // body data type must match "Content-Type" header
   });
   return response.json(); // parses JSON response into native JavaScript objects
