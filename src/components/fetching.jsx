49c40404eaffd0fd231300ed932f1924fe306395
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-01 12:37:28 +0300

    Update fetching.jsx

--- src/components/fetching.jsx
+++ src/components/fetching.jsx
@@ -2,16 +2,22 @@ import React, {Component} from 'react';
 
 export default class Fetching extends Component {
   state = {
-    items: []
+    items: [],
+    route: 'https://api-buddy.herokuapp.com/api/customers'
   };
   render() {
     return (
       <React.Fragment>
         <h1>Fetch is here!</h1>
-        <input type='text' style={{height: "200px", width: "150px"}} className="m-3"></input>
+        <input type='text' style={{height: "200px", width: "150px"}} className="m-3"
+          placeholder={this.state.route}
+          ></input>
         <input type='text' style={{height: "200px", width: "150px"}} className="m-3"></input>
         <button style={{display: "block"}} className="m-3">Fetch data</button>
       </React.Fragment>
     );
+  }
+  handleChange = (event) => {
+
   }
 }
