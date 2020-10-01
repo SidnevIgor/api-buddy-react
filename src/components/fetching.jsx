6261fa61865d487931581a5cbe591e56e9cde8a4
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-01 11:50:21 +0300

    Create fetching.jsx

---
+++ src/components/fetching.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class Fetching extends Component {
+  state = {
+    items: []
+  }
+  render() {
+    return (
+      <h1>Fetch is here!</h1>
+    );
+  }
+}
