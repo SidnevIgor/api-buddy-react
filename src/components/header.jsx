7d45c6288f1526bd5df9875d9b3429123ca2f041
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-02 16:57:52 +0300

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,35 +4,11 @@ import fetchData from '../services/fetchData';
 class Header extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-md navbar-dark bg-dark fixed-top m-2" style={{position: 'relative'}}>
-      <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-        <span className="badge badge-pill badge-secondary">
-          {this.props.totalCounters}
-        </span>
-        <ul className="navbar-nav mr-auto">
-          <li className="nav-item active">
-            <a className="nav-link" >Home <span className="sr-only">(current)</span></a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" >Link</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link disabled" >Disabled</a>
-          </li>
-          <li className="nav-item dropdown">
-            <a className="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-            <div className="dropdown-menu" aria-labelledby="dropdown01">
-              <a className="dropdown-item" >Action</a>
-              <a className="dropdown-item" >Another action</a>
-              <a className="dropdown-item" >Something else here</a>
-            </div>
-          </li>
-        </ul>
-        <form className="form-inline my-2 my-lg-0">
-          <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form>
+      <div className="col-md-5 p-lg-5 mx-auto my-5">
+        <h1 className="display-4 font-weight-normal">API-buddy</h1>
+        <p className="lead font-weight-normal">The service is a free dummy book shop API supporting GET, POST, UPDATE, DELETE requests with the following routes: https://api-buddy/customers</p>
+        <a className="btn btn-outline-secondary" href="#">Try now</a>
       </div>
-    </nav>
     );
   }
 }
