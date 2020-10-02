eb871867ee4cd8588aee62e88882c891c2e42a92
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-02 17:45:13 +0300

    Update footer.jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 export default class Footer extends Component {
   render () {
     return (
       <React.Fragment>
         <h1>Follow me</h1>
+        <FontAwesomeIcon icon={ faLinkedin } style={{color: "grey", width: "50px", height: "50px"}}/>
       </React.Fragment>
     )
   }
