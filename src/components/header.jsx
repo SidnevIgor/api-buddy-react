5ad0b20a61fa34fe7a3d526b010993cf7aab1bc0
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-03 12:59:30 +0300

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -8,8 +8,8 @@ class Header extends Component {
     return (
       <div style = {{borderColor: "black", borderStyle: "dashed",width: "100%", height: "600px", display: "inline-block", backgroundColor: "#D3E0E9", fontFamily: "Inter"}}>
         <h1 style={{marginLeft: "250px", marginTop: "50px", fontSize: "90px"}}>API-buddy</h1>
-        <p className="lead font-weight-normal" style={{marginLeft: "250px", fontSize: "30px", width:"40%"}}>The service is a free dummy book shop API supporting GET, POST, UPDATE, DELETE requests with the following routes: https://api-buddy/customers</p>
-        <a className="btn btn-outline-secondary" href="#">Try now</a>
+        <p className="lead font-weight-normal" style={{marginLeft: "250px", fontSize: "28px", width:"40%"}}>The service is a free dummy book shop API supporting GET, POST, UPDATE, DELETE requests with the following routes: https://api-buddy/customers</p>
+        <a className="btn btn-outline-secondary" style={{marginLeft: "250px", backgroundColor: "#002736", color: "white"}}>Learn more</a>
       </div>
     );
   }
