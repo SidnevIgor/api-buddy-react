6d52d18a27ff2a5769346a76ca49e3eb464d12be
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-03 19:48:22 +0300

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,19 +4,37 @@ import fetchData from '../services/fetchData';
 import './index.css';
 
 class Header extends Component {
+  state = {
+    buttonClass: "btn btn-outline-secondary"
+  };
   render() {
     return (
       <div style = {{borderColor: "black", borderStyle: "dashed",width: "100%", height: "700px", display: "inline-block", backgroundColor: "#D3E0E9", fontFamily: "Inter"}}>
         <div className="inline-container" style={{display: "inline-block", borderStyle: "dashed", width: "60%"}}>
           <h1 style={{marginLeft: "180px", marginTop: "50px", fontSize: "90px"}}>API-buddy</h1>
-          <p className="lead font-weight-normal" style={{marginLeft: "180px", fontSize: "28px", width:"70%"}}>The service is a free dummy book shop API supporting GET, POST, UPDATE, DELETE requests with the following routes: https://api-buddy/customers</p>
-          <a className="btn btn-outline-secondary" style={{marginLeft: "180px", backgroundColor: "#002736", color: "white", marginTop: "80px"}}>Learn more</a>
+          <p className="lead font-weight-normal" style={{marginLeft: "180px", fontSize: "28px", width:"65%"}}>The service is a free dummy book shop API supporting GET, POST, UPDATE, DELETE requests with the following routes: https://api-buddy/customers</p>
+          <a className={this.state.buttonClass}
+            onMouseEnter = {() => this.changeBtn(true)}
+            onMouseLeave = {() => this.changeBtn(false)}
+            style={{marginLeft: "180px", backgroundColor: "#002736", color: "white", marginTop: "80px", fontSize: "20px"}}>Learn more</a>
         </div>
         <div className="inline-container" style={{display: "inline-block"}}>
           <img src={require('./images/header_image.png')} alt="api-buddy-header-image" width="600px" height="500px"/>
         </div>
       </div>
     );
+  }
+  changeBtn = (open) => {
+    if(open) {
+      this.setState({
+        buttonClass: this.state.buttonClass+=' rounded-pill'
+      })
+    }
+    else {
+      this.setState({
+        buttonClass: "btn btn-outline-secondary"
+      })
+    }
   }
 }
 export default Header;
