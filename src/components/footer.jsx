01e47bcc02768506c35597ff7a6199cffd610ef3
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-07 13:44:07 +0300

    Adding new icons for footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import { faFacebookSquare } from '@fortawesome/free-brands-svg-icons';
 
 export default class Footer extends Component {
   render () {
     return (
       <React.Fragment>
-        <FontAwesomeIcon icon={ faLinkedin } style={{color: "grey", width: "50px", height: "50px"}}/>
-        <FontAwesomeIcon icon={ faFacebookSquare } style={{color: "grey", width: "50px", height: "50px"}}/>
+        
       </React.Fragment>
     )
   }
