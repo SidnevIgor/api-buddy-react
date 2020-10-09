e70ebf88c5d59620fb21254c7d01d127018b3a82
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-09 18:13:37 +0300

    Update routesDisplay.jsx

--- src/components/routesDisplay.jsx
+++ src/components/routesDisplay.jsx
@@ -3,8 +3,26 @@ import React, { Component } from 'react';
 export default class RoutesDisplay extends Component {
   render() {
     return (
-      <div>
+      <div style={{fontFamily: "Inter", backgroundColor: "#E9EEF2", height: "600px", textAlign: "center"}}>
         <h1>Available routes</h1>
+        <table style={{width: "500px", borderStyle: "solid", textAlign: "left"}}>
+          <tr>
+            <td rowspan="5">https://api-buddy</td>
+            <td>/customers</td>
+          </tr>
+          <tr>
+            <td>/employees</td>
+          </tr>
+          <tr>
+            <td>/stores</td>
+          </tr>
+          <tr>
+            <td>/books</td>
+          </tr>
+          <tr>
+            <td>/orders</td>
+          </tr>
+        </table>
       </div>
     )
   }
