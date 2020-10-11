a5b778e3f65a34b0803de790d0d8bb156e6cfb1f
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-11 17:30:25 +0300

    Update routesDisplay.jsx

--- src/components/routesDisplay.jsx
+++ src/components/routesDisplay.jsx
@@ -6,26 +6,26 @@ export default class RoutesDisplay extends Component {
       <div style={{fontFamily: "Inter", backgroundColor: "#E9EEF2", height: "600px", textAlign: "center"}}>
         <h1>Available routes</h1>
         <div style={{width: "100%", textAlign: "center", borderStyle: "dashed"}}>
-          <table style={{width: "21%", borderStyle: "solid", textAlign: "left", display: "inline-block", fontSize: "20px", color: "#314C5B"}} className="m-5" id = "mainTable">
+          <table style={{width: "23%", borderStyle: "solid", textAlign: "left", display: "inline-block", fontSize: "20px", color: "#314C5B"}} className="m-5" id = "mainTable">
             <tbody>
               <tr>
-                <td className = "reqList" id = "row11"></td>
+                <td className = "reqList p-3" id = "row11"></td>
                 <td className = "reqOption p-3" id = "row12" onMouseEnter = {(event) => {this.changeLine(event.target.id)}}>/customers</td>
               </tr>
               <tr>
-                <td className = "reqList" id = "row21"></td>
+                <td className = "reqList p-3" id = "row21"></td>
                 <td className = "reqOption p-3" id = "row22" onMouseEnter = {(event) => {this.changeLine(event.target.id)}}>/employees</td>
               </tr>
               <tr>
-                <td className = "reqList" id = "row31">https://api-buddy</td>
+                <td className = "reqList p-3" id = "row31">https://api-buddy</td>
                 <td className = "reqOption p-3" id = "row32" onMouseEnter = {(event) => {this.changeLine(event.target.id)}}>/stores</td>
               </tr>
               <tr>
-                <td className = "reqList" id = "row41"></td>
+                <td className = "reqList p-3" id = "row41"></td>
                 <td className = "reqOption p-3" id = "row42" onMouseEnter = {(event) => {this.changeLine(event.target.id)}}>/books</td>
               </tr>
               <tr>
-                <td className = "reqList" id = "row51"></td>
+                <td className = "reqList p-3" id = "row51"></td>
                 <td className = "reqOption p-3" id = "row52" onMouseEnter = {(event) => {this.changeLine(event.target.id)}}>/orders</td>
               </tr>
             </tbody>
