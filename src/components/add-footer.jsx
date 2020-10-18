a0654a0350c734c4db4bcdd2d116d8fa1f9e1c5d
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-18 17:37:56 +0300

    Update add-footer.jsx

--- src/components/add-footer.jsx
+++ src/components/add-footer.jsx
@@ -1,5 +1,14 @@
 import React, { Component } from 'react';
 
 export default class AddFooter extends Component {
-  
+  state = {
+
+  };
+  render() {
+    return (
+      <div style = {{backgroundColor: "#002736", textAlign: "center"}}>
+        
+      </div>
+    );
+  }
 }
