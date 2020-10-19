1883531e1aecfc415462c6e2452372b688a09063
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-19 14:57:52 +0300

    Update add-footer.jsx

--- src/components/add-footer.jsx
+++ src/components/add-footer.jsx
@@ -9,6 +9,11 @@ export default class AddFooter extends Component {
       <div style = {{backgroundColor: "#002736", textAlign: "center"}}>
         <div style = {{ borderTop: "10px dashed yellow" }}>
         </div>
+        <ol style = {{ color: "white" }}>
+          <li style = {{ color: "red" }}>First</li>
+          <li>Second</li>
+          <li>Third</li>
+        </ol>
       </div>
     );
   }
