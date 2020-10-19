09d3a338f5aaa6dd51395895b137796161dc3568
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-19 19:52:21 +0300

    Adding headings

--- src/components/add-footer.jsx
+++ src/components/add-footer.jsx
@@ -7,15 +7,19 @@ export default class AddFooter extends Component {
   render() {
     return (
       <div style = {{backgroundColor: "#002736", textAlign: "center"}}>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, iste iusto quas eligendi corporis id eius corrupti
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, iste iusto quas eligendi corporis id eius corrupti
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, iste iusto quas eligendi corporis id eius corrupti
-        </p>
+        <div style = {{ width: "80%" }} className = "middle-align">
+          <p>
+            <h3>Get a JWT token by fetching https://api-buddy.com/api/auth</h3>
+          </p>
+          <p>
+            <h3>
+              Fetch a resource with a Header parameter 'x-auth-token'
+            </h3>
+          </p>
+          <p>
+            <h3>Get a resulting JSON</h3>
+          </p>
+        </div>
       </div>
     );
   }
