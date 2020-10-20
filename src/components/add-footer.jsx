6c8ec721f2bb3e7a662a0929dfeafa6d43b07de1
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-20 16:33:09 +0300

    Update add-footer.jsx

--- src/components/add-footer.jsx
+++ src/components/add-footer.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 export default class AddFooter extends Component {
   state = {
-
+    query: 'fetch(https://api-buddy.herokuapp.com/api/auth)\n  .then((data) => console.log(data))',
+    items: [1,2,3]
   };
   render() {
     return (
@@ -12,6 +13,23 @@ export default class AddFooter extends Component {
         <div style = {{ width: "80%" }} className = "middle-align">
           <p className = "first-p">
             <h3>Get a JWT token by fetching https://api-buddy.com/api/auth</h3>
+              <div className="d-flex justify-content-center">
+
+                <textarea type='text' style={{ height: "400px", width: "500px", backgroundColor: 'white', color: 'black', resize: "none", fontSize: "18px" }}
+                  data-gramm_editor="false"
+                  className="m-5"
+                  value={this.state.query}
+                  onChange={(event) => this.handleChange(event)}
+                ></textarea>
+
+              <textarea type='text' style={{ height: "400px", width: "500px", backgroundColor: 'white', color: 'black', resize: "none", fontSize: "18px" }}
+                  data-gramm_editor="false"
+                  className="m-5"
+                  value={this.state.items}
+                  onChange={() => {}}
+                ></textarea>
+
+              </div>
           </p>
           <p>
             <h3>
