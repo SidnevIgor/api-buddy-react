0ceab227ca5b368c7e1a33bd575935d7000614f2
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-21 00:12:02 +0300

    Update add-footer.jsx

--- src/components/add-footer.jsx
+++ src/components/add-footer.jsx
@@ -6,7 +6,8 @@ export default class AddFooter extends Component {
     query: "fetch(https://api-buddy.herokuapp.com/api/auth, {\n  method: 'POST'\n})\n  .then((data) => console.log(data))",
     items: {
       'x-auth-token':'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI1Zjc1NzkxMDA5M2Q4NjAwMTc5NzA5NTMiLCJpYXQiOjE2MDE1MzQyMjV9.PjPqjLi221SN6TgapD4USzUdm-wHE2H_hGC8n_R3ORE'
-    }
+    },
+    itemsNew: "fetch(https://api-buddy.herokuapp.com/api/auth, {\n  method: 'POST'\n})\n  .then((data) => console.log(data))"
   };
   render() {
     return (
@@ -19,10 +20,10 @@ export default class AddFooter extends Component {
               <div className="d-flex justify-content-center">
 
                 <textarea type='text' style={{ height: "400px", width: "500px", backgroundColor: 'white', color: 'black', resize: "none", fontSize: "18px" }}
-                  data-gramm_editor="false"
-                  className="m-5"
+                  data-gramm_editor = "false"
+                  className = "m-5"
                   value = { this.state.query }
-                  onChange={(event) => this.handleChange(event)}
+                  onChange = {(event) => this.handleChange(event)}
                 ></textarea>
 
               <textarea type='text' style={{ height: "400px", width: "500px", backgroundColor: 'white', color: 'black', resize: "none", fontSize: "18px" }}
@@ -38,6 +39,14 @@ export default class AddFooter extends Component {
             <h3>
               Fetch a resource with a Header parameter 'x-auth-token'
             </h3>
+            <div className = "d-flex justify-content-center">
+              <textarea type='text' style={{ height: "400px", width: "100%", backgroundColor: 'white', color: 'black', resize: "none", fontSize: "18px" }}
+                  data-gramm_editor = "false"
+                  className = "m-5"
+                  value = { this.state.query }
+                  onChange = {() => {}}
+                ></textarea>
+            </div>
           </p>
           <p>
             <h3>Get a resulting JSON</h3>
