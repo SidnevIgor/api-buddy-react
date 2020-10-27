3e484540f2aa1947d30b0283a91f8413729b5b88
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-27 09:52:53 +0300

    Hovering effect

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import './index.css';
 
 export default class Social extends Component {
   render () {
     return (
       <div style = {{backgroundColor: "#002736", textAlign: "right"}}>
         <a className = "m-1" alt="LinkedIn page" width="40" height="40" href="https://www.linkedin.com/in/igor-sidnev/">
-          <img src={require("./images/linkedin.png")} width="36" height="36" alt="linkedIn page"/>
+          <img className = "social" src={require("./images/linkedin.png")} width="36" height="36" alt="linkedIn page"/>
         </a>
         <a className = "m-1" background = { require("./images/footer_facebook.png")} alt="Facebook page" width="40" height="40"
           href="https://www.facebook.com/profile.php?id=100006233297824">
-          <img src={ require("./images/facebook.png") } width="30" height="30" alt="facebook page" style = {{color: "white" }}/>
+          <img className = "social" src={ require("./images/facebook.png") } width="30" height="30" alt="facebook page" style = {{color: "white" }}/>
         </a>
       </div>
     )
