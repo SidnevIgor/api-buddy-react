53edb262f51ce36fb1bfae14c47d5b910c7cc42b
Igor Sidnev <44197959+SidnevIgor@users.noreply.github.com>
2020-10-27 09:16:42 +0300

    Fixing icons' sizes

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,12 +4,12 @@ export default class Social extends Component {
   render () {
     return (
       <div style = {{backgroundColor: "#002736", textAlign: "right"}}>
-        <a alt="LinkedIn page" width="40" height="40" href="https://www.linkedin.com/in/igor-sidnev/">
-          <img src={require("./images/footer_linkedin.png")} width="40" height="40" alt="linkedIn page"/>
+        <a className = "m-1" alt="LinkedIn page" width="40" height="40" href="https://www.linkedin.com/in/igor-sidnev/">
+          <img src={require("./images/linkedin.png")} width="36" height="36" alt="linkedIn page"/>
         </a>
-        <a background = { require("./images/footer_facebook.png")} alt="Facebook page" width="40" height="40"
+        <a className = "m-1" background = { require("./images/footer_facebook.png")} alt="Facebook page" width="40" height="40"
           href="https://www.facebook.com/profile.php?id=100006233297824">
-          <img src={require("./images/footer_facebook.png")} width="40" height="40" alt="facebook page"/>
+          <img src={ require("./images/facebook.png") } width="30" height="30" alt="facebook page" style = {{color: "white" }}/>
         </a>
       </div>
     )
